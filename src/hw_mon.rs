aa2d0772c2475d3b4a574d77a9e32458bb9a17da
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-06-19 19:10:06 +0300

    Add power cap reading

--- src/hw_mon.rs
+++ src/hw_mon.rs
@@ -63,7 +63,8 @@ impl HwMon {
     pub fn get_gpu_clockspeed(&self) -> Option<u64> {
         self.read_file("freq1_input").map(|f| {
             f.parse::<u64>()
-                .expect("Unexpected GPU clockspeed (driver bug?)") / 1000000
+                .expect("Unexpected GPU clockspeed (driver bug?)")
+                / 1000000
         })
     }
 
@@ -71,7 +72,44 @@ impl HwMon {
     pub fn get_vram_clockspeed(&self) -> Option<u64> {
         self.read_file("freq2_input").map(|f| {
             f.parse::<u64>()
-                .expect("Unexpected VRAM clockspeed (driver bug?)") / 1000000
+                .expect("Unexpected VRAM clockspeed (driver bug?)")
+                / 1000000
+        })
+    }
+
+    /// Gets the average power (currently) used by the GPU in watts.
+    pub fn get_power_average(&self) -> Option<f64> {
+        self.read_file("power1_average").map(|p| {
+            p.parse::<f64>()
+                .expect("Unexpected power usage (driver bug?)")
+                / 1000000.0
+        })
+    }
+
+    /// Gets the current power cap of the GPU in watts.
+    pub fn get_power_cap(&self) -> Option<f64> {
+        self.read_file("power1_cap").map(|p| {
+            p.parse::<f64>()
+                .expect("Unexpected power usage (driver bug?)")
+                / 1000000.0
+        })
+    }
+
+    /// Gets the maximum possible power cap for the GPU in watts. If overclocking is disabled, this is probably the same as the default cap.
+    pub fn get_power_cap_max(&self) -> Option<f64> {
+        self.read_file("power1_cap_max").map(|p| {
+            p.parse::<f64>()
+                .expect("Unexpected power usage (driver bug?)")
+                / 1000000.0
+        })
+    }
+
+    /// Gets the minimum possible power cap for the GPU in watts.
+    pub fn get_power_cap_min(&self) -> Option<f64> {
+        self.read_file("power1_cap_min").map(|p| {
+            p.parse::<f64>()
+                .expect("Unexpected power usage (driver bug?)")
+                / 1000000.0
         })
     }
 }
