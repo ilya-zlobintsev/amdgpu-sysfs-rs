5706e1bc040ab00e7348cdcad0fef9e9ed1a7b6a
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-06-19 18:59:59 +0300

    Add sysfs write

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -3,10 +3,16 @@ use std::path::Path;
 pub trait SysFS {
     fn get_path(&self) -> &Path;
 
+    /// Reads the content of a file in the SysFS.
     fn read_file(&self, file: &str) -> Option<String> {
         match std::fs::read_to_string(self.get_path().join(file)) {
             Ok(contents) => Some(contents.trim().to_string()),
             Err(_) => None,
         }
+    }
+
+    /// Write to a file in the SysFS. 
+    fn write_file(&self, file: &str, contents: &str) -> Result<(), std::io::Error> {
+        std::fs::write(self.get_path().join(file), contents)
     }
 }
