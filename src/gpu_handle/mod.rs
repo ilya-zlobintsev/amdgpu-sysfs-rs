7148740aa310a55150e0817735f43fa2faf23124
Ilya Zlobintsev <ilya.zl@protonmail.com>
2024-08-18 21:28:45 +0300

    feat: support for parsing values within power profiles (`pp_power_profile_mode`) (#9)
    * feat: initial support for parsing values in power profiles
    
    * feat: parse nested values, basic support for setting values
    
    * chore: rename field, bump version
    
    * chore: comment out incomplete custom value functionality
    
    * accept snapshot changes

--- src/gpu_handle/mod.rs
+++ src/gpu_handle/mod.rs
@@ -114,7 +114,7 @@ impl GpuHandle {
         let mut sysfs_path = std::fs::canonicalize(self.get_path())?.join("../"); // pcie port
 
         for _ in 0..2 {
-            let Ok(did) = std::fs::read_to_string(&sysfs_path.join("device")) else {
+            let Ok(did) = std::fs::read_to_string(sysfs_path.join("device")) else {
                 break;
             };
 
@@ -317,6 +317,34 @@ impl GpuHandle {
     pub fn set_active_power_profile_mode(&self, i: u16) -> Result<()> {
         self.write_file("pp_power_profile_mode", format!("{i}\n"))
     }
+
+    /*/// Sets a custom power profile mode. You can get the available modes, and the list of heuristic names with [`get_power_profile_modes`].
+    /// Requires the performance level to be set to "manual" first using [`set_power_force_performance_level`]
+    pub fn set_custom_power_profile_mode(&self, heuristics: Vec<Option<i32>>) -> Result<()> {
+        let table = self.get_power_profile_modes()?;
+        let (index, custom_profile) = table
+            .modes
+            .iter()
+            .find(|(_, profile)| profile.name == "CUSTOM")
+            .ok_or_else(|| {
+                ErrorKind::NotAllowed("Could not find a custom power profile".to_owned())
+            })?;
+
+        if custom_profile.components.len() == 1 {
+            let mut values_command = format!("{index}");
+            for heuristic in heuristics {
+                match heuristic {
+                    Some(value) => write!(values_command, " {value}").unwrap(),
+                    None => write!(values_command, " -").unwrap(),
+                }
+            }
+
+            values_command.push('\n');
+            self.write_file("pp_power_profile_mode", values_command)
+        } else {
+            todo!();
+        }
+    }*/
 
     fn read_fan_info(&self, file: &str, section_name: &str, range_name: &str) -> Result<FanInfo> {
         let file_path = self.get_path().join("gpu_od/fan_ctrl").join(file);
--- src/gpu_handle/power_profile_mode.rs
+++ src/gpu_handle/power_profile_mode.rs
@@ -1,7 +1,9 @@
 //! `pp-power-profile-mode`
-
-use super::trim_sysfs_line;
-use crate::{error::Error, Result};
+#![allow(missing_docs)] // temp
+use crate::{
+    error::{Error, ErrorKind},
+    Result,
+};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
@@ -13,47 +15,276 @@ use std::collections::BTreeMap;
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct PowerProfileModesTable {
     /// List of available modes
-    pub modes: BTreeMap<u16, String>,
+    pub modes: BTreeMap<u16, PowerProfile>,
+    /// Names for the values in [`PowerProfile`]
+    pub value_names: Vec<String>,
     /// The currently active mode
     pub active: u16,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct PowerProfile {
+    pub name: String,
+    /// On RDNA and newer, each profile has multiple components for different clock types.
+    /// Older generations have only one set of values.
+    pub components: Vec<PowerProfileComponent>,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct PowerProfileComponent {
+    /// Filled on RDNA and newer
+    pub clock_type: Option<String>,
+    pub values: Vec<Option<i32>>,
 }
 
 impl PowerProfileModesTable {
     /// Parse the table from a given string
     pub fn parse(s: &str) -> Result<Self> {
+        let mut split = s.split_whitespace();
+        let start = split
+            .next()
+            .ok_or_else(|| Error::unexpected_eol("Value description", 1))?;
+
+        match start {
+            "NUM" => Self::parse_flat(s),
+            "PROFILE_INDEX(NAME)" => Self::parse_nested(s),
+            _ if start.parse::<u16>().is_ok() => Self::parse_basic(s),
+            _ => Err(Error::basic_parse_error(
+                "Could not determine the type of power profile mode table",
+            )),
+        }
+    }
+
+    /// Parse the format used by pre-RDNA GPUs
+    fn parse_flat(s: &str) -> Result<Self> {
         let mut modes = BTreeMap::new();
         let mut active = None;
 
-        for (line, row) in s.lines().map(trim_sysfs_line).enumerate() {
-            let mut parts = row.split_whitespace();
+        let mut lines = s.lines();
 
-            if let Some(num) = parts.next().and_then(|part| part.parse::<u16>().ok()) {
-                let mut name = parts
+        let header_line = lines
+            .next()
+            .ok_or_else(|| Error::unexpected_eol("Info header", 1))?;
+        let mut header_split = header_line.split_whitespace();
+
+        if header_split.next() != Some("NUM") {
+            return Err(
+                ErrorKind::Unsupported("Expected header to start with 'NUM'".to_owned()).into(),
+            );
+        }
+        if header_split.next() != Some("MODE_NAME") {
+            return Err(ErrorKind::Unsupported(
+                "Expected header to contain 'MODE_NAME'".to_owned(),
+            )
+            .into());
+        }
+
+        let value_names: Vec<String> = header_split.map(str::to_owned).collect();
+
+        for (line, row) in s.lines().map(str::trim).enumerate() {
+            let mut split = row.split_whitespace().peekable();
+            if let Some(num) = split.next().and_then(|part| part.parse::<u16>().ok()) {
+                let name_part = split
                     .next()
-                    .ok_or_else(|| Error::unexpected_eol("No name after mode number", line))?
-                    .trim_matches(':');
+                    .ok_or_else(|| Error::unexpected_eol("Mode name", line + 1))?
+                    .trim_end_matches(':');
 
-                if let Some(stripped_name) = name.strip_suffix('*') {
-                    name = stripped_name;
-                    active = Some(num);
-                }
-
-                modes.insert(num, name.to_owned());
-
-                // To handle cases that look like this:
-                // 1 3D_FULL_SCREEN *:
-                if active.is_none() {
-                    if let Some(part) = parts.next() {
-                        if part.starts_with('*') {
+                // Handle space within the mode name:
+                // `3D_FULL_SCREEN *:`
+                if let Some(next) = split.peek() {
+                    if next.ends_with(':') {
+                        if next.starts_with('*') {
                             active = Some(num);
                         }
+                        split.next();
                     }
                 }
+
+                let name = if let Some(name) = name_part.strip_suffix('*') {
+                    active = Some(num);
+                    name.trim()
+                } else {
+                    name_part
+                };
+
+                let values = split
+                    .map(|value| {
+                        if value == "-" {
+                            Ok(None)
+                        } else {
+                            let parsed = value.parse().map_err(|_| {
+                                Error::from(ErrorKind::ParseError {
+                                    msg: format!("Expected an integer, got '{value}'"),
+                                    line: line + 1,
+                                })
+                            })?;
+                            Ok(Some(parsed))
+                        }
+                    })
+                    .collect::<Result<_>>()?;
+
+                let power_profile = PowerProfile {
+                    name: name.to_owned(),
+                    components: vec![PowerProfileComponent {
+                        clock_type: None,
+                        values,
+                    }],
+                };
+                modes.insert(num, power_profile);
             }
         }
 
         Ok(Self {
             modes,
+            value_names,
+            active: active.ok_or_else(|| Error::basic_parse_error("No active level found"))?,
+        })
+    }
+
+    /// Parse the format used by RDNA and higher
+    fn parse_nested(s: &str) -> Result<Self> {
+        let mut modes = BTreeMap::new();
+        let mut active = None;
+
+        let mut lines = s.lines();
+
+        let header_line = lines
+            .next()
+            .ok_or_else(|| Error::unexpected_eol("Info header", 1))?;
+        let mut header_split = header_line.split_whitespace();
+
+        if header_split.next() != Some("PROFILE_INDEX(NAME)") {
+            return Err(ErrorKind::Unsupported(
+                "Expected header to start with 'PROFILE_INDEX(NAME)'".to_owned(),
+            )
+            .into());
+        }
+        if header_split.next() != Some("CLOCK_TYPE(NAME)") {
+            return Err(ErrorKind::Unsupported(
+                "Expected header to contain 'CLOCK_TYPE(NAME)'".to_owned(),
+            )
+            .into());
+        }
+
+        let value_names: Vec<String> = header_split.map(str::to_owned).collect();
+
+        let mut lines = lines.map(str::trim).enumerate().peekable();
+        while let Some((line, row)) = lines.next() {
+            if row.contains('(') {
+                return Err(ErrorKind::ParseError {
+                    msg: format!("Unexpected mode heuristics line '{row}'"),
+                    line: line + 1,
+                }
+                .into());
+            }
+
+            let mut split = row.split_whitespace();
+            if let Some(num) = split.next().and_then(|part| part.parse::<u16>().ok()) {
+                let name_part = split
+                    .next()
+                    .ok_or_else(|| Error::unexpected_eol("No name after mode number", line + 1))?
+                    .trim_end_matches(':');
+
+                let name = if let Some(name) = name_part.strip_suffix('*') {
+                    active = Some(num);
+                    name.trim()
+                } else {
+                    name_part
+                };
+
+                let mut components = Vec::new();
+
+                while lines
+                    .peek()
+                    .is_some_and(|(_, row)| row.contains(|c| c == '(' || c == ')'))
+                {
+                    let (line, clock_type_line) = lines.next().unwrap();
+
+                    let name_start = clock_type_line
+                        .char_indices()
+                        .position(|(_, c)| c == '(')
+                        .ok_or_else(|| Error::unexpected_eol('(', line + 1))?;
+
+                    let name_end = clock_type_line
+                        .char_indices()
+                        .position(|(_, c)| c == ')')
+                        .ok_or_else(|| Error::unexpected_eol(')', line + 1))?;
+
+                    let clock_type = clock_type_line[name_start + 1..name_end].trim();
+
+                    let clock_type_values = clock_type_line[name_end + 1..]
+                        .split_whitespace()
+                        .map(str::trim)
+                        .map(|value| {
+                            if value == "-" {
+                                Ok(None)
+                            } else {
+                                let parsed = value.parse().map_err(|_| {
+                                    Error::from(ErrorKind::ParseError {
+                                        msg: format!("Expected an integer, got '{value}'"),
+                                        line: line + 1,
+                                    })
+                                })?;
+                                Ok(Some(parsed))
+                            }
+                        })
+                        .collect::<Result<Vec<Option<i32>>>>()?;
+
+                    components.push(PowerProfileComponent {
+                        clock_type: Some(clock_type.to_owned()),
+                        values: clock_type_values,
+                    })
+                }
+
+                let power_profile = PowerProfile {
+                    name: name.to_owned(),
+                    components,
+                };
+                modes.insert(num, power_profile);
+            }
+        }
+
+        Ok(Self {
+            modes,
+            value_names,
+            active: active.ok_or_else(|| Error::basic_parse_error("No active level found"))?,
+        })
+    }
+
+    /// Parse the format used by integrated GPUs
+    fn parse_basic(s: &str) -> Result<Self> {
+        let mut modes = BTreeMap::new();
+        let mut active = None;
+
+        for (line, row) in s.lines().map(str::trim).enumerate() {
+            let mut split = row.split_whitespace();
+            if let Some(num) = split.next().and_then(|part| part.parse::<u16>().ok()) {
+                let name_part = split
+                    .next()
+                    .ok_or_else(|| Error::unexpected_eol("No name after mode number", line + 1))?;
+
+                let name = if let Some(name) = name_part.strip_suffix('*') {
+                    active = Some(num);
+                    name
+                } else {
+                    name_part
+                };
+
+                modes.insert(
+                    num,
+                    PowerProfile {
+                        name: name.to_owned(),
+                        components: vec![],
+                    },
+                );
+            }
+        }
+
+        Ok(Self {
+            modes,
+            value_names: vec![],
             active: active.ok_or_else(|| Error::basic_parse_error("No active level found"))?,
         })
     }
